567422c75caa92727f0bd717c0f0caa1982250af
Trevor M <metatrevor@gmail.com>
2014-12-10 19:39:13 +0000

    Login functions & template

--- static/javascripts/authentication/services/authentication.service.js
+++ static/javascripts/authentication/services/authentication.service.js
@@ -22,7 +22,12 @@
          */
 
         var Authentication = {
-            register: register
+            register: register,
+            getAuthenticatedAccount : getAuthenticatedAccount,
+            login : login,
+            isAuthenticated : isAuthenticated,
+            setAuthenticatedAccount: setAuthenticatedAccount,
+            unauthenticated : unauthenticate
         };
 
         return Authentication;
@@ -42,6 +47,83 @@
                 password: password,
                 email: email
             }).then();
+        }
+
+        /**
+         * @name login
+         * @desc login with email and password
+         * @param email
+         * @param password
+         * @returns {Promise}
+         */
+        function login(email, password) {
+            return $http.post('/api/v1/auth/login/', {
+                email : email,
+                password : password
+            }).then(loginSuccessFn, loginErrorFn);
+        }
+
+        /**
+         * @name getAuthenticatedAccount
+         * @desc return the currently authenticated account
+         * @returns {object|undefined} if authenticated else undefined
+         * @memberOf social.authentication.services.Authentication
+         */
+
+        function getAuthenticatedAccount() {
+            if(!$cookies.authenticatedAccount) {
+                return;
+            }
+            return JSON.parse($cookies.authenticatedAccount);
+        }
+
+       /**
+         * @name setAuthenticatedUser
+         * @desc Stringify the account object and store it in a cookie
+         * @param {Object} account
+         * @returns {undefined}
+         * @memberOf social.authentication.services.Authentication
+         */
+        function setAuthenticatedAccount(account) {
+          $cookies.authenticatedAccount = JSON.stringify(account);
+        }
+
+        /**
+         * @name isAuthenticated
+         * @desc check if the current user is authenticated
+         * @returns true if authenticated else false
+         */
+
+        function isAuthenticated() {
+            return !!$cookies.authenticatedAccount;
+        }
+
+        /**
+         * @name unauthenticate
+         * @desc Delete the cookie where the user object is stored
+         * @returns {undefined}
+         * @memberOf social.authentication.services.Authentication
+         */
+        function unauthenticate() {
+            delete $cookies.authenitcatedAccount;
+        }
+
+        /**
+         * @name loginSuccessFn
+         * @desc set the authenticated account and redirect to index
+         */
+
+        function loginSuccessFn(data, status, headers, config) {
+            Authentication.setAuthenticatedAccount(data);
+            window.location = '/';
+        }
+
+        /**
+         * @name loginErrorFn
+         * @desc Log "Login failure"
+         */
+        function loginErrorFn(data, status, headers, config) {
+            console.error('Login failure');
         }
     }
 })();
