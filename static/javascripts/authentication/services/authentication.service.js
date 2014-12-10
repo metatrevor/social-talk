d065a2f7e24e2b988015f69c9af4d28de03cc0f6
Trevor M <metatrevor@gmail.com>
2014-12-10 09:35:52 +0000

    Implement angular authentication factory

--- static/javascripts/authentication/services/authentication.service.js
+++ static/javascripts/authentication/services/authentication.service.js
@@ -6,5 +6,43 @@
 
 (function(){
     'use strict';
+    angular
+        .module('social.authentication.services')
+        .factory('Authentication', Authentication);
 
-});
+    Authentication.$inject = ['$cookies', '$http'];
+
+    /**
+     * @namespace authentication
+     * @returns {Factory}
+     */
+    function Authentication($cookies, $http) {
+        /**
+         * @name Authentication
+         * @desc The factory to be returned
+         */
+
+        var Authentication = {
+            register: register
+        };
+
+        return Authentication;
+
+        /**
+         * @name register
+         * @desc Register a new user
+         * @param {string} username
+         * @param {string} password
+         * @param {string} email
+         * @returns {Promise}
+         * @memberOf social.authentication.services.Authentication
+         */
+        function register(email, password, username) {
+            return $http.post('/api/v1/accounts', {
+                username : username,
+                password : password,
+                email : email
+            });
+        }
+    }
+})();
