8e58fe6f45278a02c3b461fc1f0b6372976960ed
Trevor M <metatrevor@gmail.com>
2014-12-10 16:13:49 +0000

    Url fixes

--- social-django-angular/urls.py
+++ social-django-angular/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, url, include
-from rest_framework import routers
+from rest_framework_nested import routers
 from authentication.views import AccountViewSet
 from .views import IndexView
 
@@ -8,6 +8,6 @@ router.register(r'accounts', AccountViewSet)
 
 urlpatterns = patterns(
     '',
-    url(r'api/v1/', include(router.urls)),
+    url(r'^api/v1/', include(router.urls)),
     url('^.*$', IndexView.as_view(), name='index'),
 )
