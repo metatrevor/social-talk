710de200f163640f87d2ad7ffa255054307bac3d
Trevor M <metatrevor@gmail.com>
2014-12-10 07:59:17 +0000

    User model creation & migration

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,81 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
+from django.core import validators
+from django.utils import timezone
+from django.utils.http import urlquote
+from django.core.mail import send_mail
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Account(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField('email address', blank=False, unique=True)
+    first_name = models.CharField(_('first name'), max_length=40, blank=True, null=True, unique=False)
+    last_name = models.CharField(_('last name'), max_length=40, blank=True, null=True, unique=False)
+    username = models.CharField('user name', max_length=20, blank=True, null=True, unique=True,
+        help_text=_('Required. 30 characters or fewer. Letters, numbers and @/./+/-/_ characters'),
+        validators=[validators.RegexValidator(regex='^[a-zA-Z0-9]*$', message='Username must be Alphanumeric', code='invalid_username')
+        ])
+    is_staff = models.BooleanField(_('staff status'), default=False,
+        help_text=_('Designates whether the user can log into this admin '
+                    'site.'))
+    is_active = models.BooleanField(_('active'), default=True,
+        help_text=_('Designates whether this user should be treated as '
+                    'active. Unselect this instead of deleting accounts.'))
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+        abstract = False
+
+    def get_absolute_url(self):
+        return "/users/%s/" % urlquote(self.username)
+
+    @property
+    def name(self):
+        if self.first_name:
+            return self.first_name
+        elif self.display_name:
+            return self.display_name
+        return 'You'
+
+    def get_full_name(self):
+        """
+        Returns the first_name plus the last_name, with a space in between.
+        """
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        "Returns the short name for the user."
+        return self.first_name
+
+    def guess_display_name(self):
+        """Set a display name, if one isn't already set."""
+        if self.display_name:
+            return
+
+        if self.first_name and self.last_name:
+            dn = "%s %s" % (self.first_name, self.last_name[0])
+        elif self.first_name:
+            dn = self.first_name
+        else:
+            dn = 'You'
+        self.display_name = dn.strip()
+
+    def email_user(self, subject, message, from_email=None):
+        """
+        Sends an email to this User.
+        """
+        send_mail(subject, message, from_email, [self.email])
+
+    def __str__(self):
+        return self.email
+
+    def natural_key(self):
+        return (self.email,)
