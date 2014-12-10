fe12992c0b78bd017377d35396aa5663dc7abefa
Trevor M <metatrevor@gmail.com>
2014-12-10 09:20:12 +0000

    Account View Set API endpoint

--- social-django-angular/urls.py
+++ social-django-angular/urls.py
@@ -1,9 +1,13 @@
-from django.conf.urls import patterns, url
-
+from django.conf.urls import patterns, url, include
+from rest_framework import routers
+from authentication.views import AccountViewSet
 from .views import IndexView
+
+router = routers.SimpleRouter()
+router.register(r'accounts', AccountViewSet)
 
 urlpatterns = patterns(
     '',
-
+    url(r'api/v1/', include(router.urls)),
     url('^.*$', IndexView.as_view(), name='index'),
 )
