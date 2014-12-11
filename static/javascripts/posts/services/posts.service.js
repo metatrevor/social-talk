994c6ad00cd136768f3ce06a97f0cbdcf74ecb03
Trevor M <metatrevor@gmail.com>
2014-12-11 10:00:51 +0000

    Post service and template

---
+++ static/javascripts/posts/services/posts.service.js
@@ -0,0 +1,64 @@
+/**
+* Posts
+* @namespace social.posts.services
+*/
+(function () {
+  'use strict';
+
+  angular
+    .module('social.posts.services')
+    .factory('Posts', Posts);
+
+  Posts.$inject = ['$http'];
+
+  /**
+  * @namespace Posts
+  * @returns {Factory}
+  */
+  function Posts($http) {
+    var Posts = {
+      all: all,
+      create: create,
+      get: get
+    };
+
+    return Posts;
+
+    ////////////////////
+
+    /**
+    * @name all
+    * @desc Get all Posts
+    * @returns {Promise}
+    * @memberOf social.posts.services.Posts
+    */
+    function all() {
+      return $http.get('/api/v1/posts/');
+    }
+
+
+    /**
+    * @name create
+    * @desc Create a new Post
+    * @param {string} content
+    * @returns {Promise}
+    * @memberOf social.posts.services.Posts
+    */
+    function create(content) {
+      return $http.post('/api/v1/posts/', {
+        content: content
+      });
+    }
+
+    /**
+     * @name get
+     * @desc Get the Posts of a given user
+     * @param {string} username
+     * @returns {Promise}
+     * @memberOf social.posts.services.Posts
+     */
+    function get(username) {
+      return $http.get('/api/v1/accounts/' + username + '/posts/');
+    }
+  }
+})();
