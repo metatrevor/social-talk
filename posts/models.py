5f8dd13ad1e77eff914c86d4fa86191608be4fb4
Trevor M <metatrevor@gmail.com>
2014-12-11 09:31:15 +0000

    Post app, model and migrations

---
+++ posts/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from authentication.models import Account
+
+class Post(models.Model):
+    author = models.ForeignKey(Account)
+    content = models.TextField()
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return '{0}'.format(self.content)
